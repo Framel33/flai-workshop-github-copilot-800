90d3f403d92db9392a267c6f9c0a5c3b40b57b5d
Framel33 <114592869+Framel33@users.noreply.github.com>
2026-02-11 10:37:36 +0000

    fix: Correct comment formatting for production debug warning in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-u6#asvaf@qe1iq)f$_5s7ze27y$8qd3q$@-*4$cvwfx+d=abmz'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! #
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
